c3ba3b150944c67cc40a8bfc93f51dc33bd428a3
fr4nsyz <christien.coleongco@gmail.com>
2026-03-17 22:27:02 -0400

    add lizard

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_1ttgGN from "../../assets/span.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
+export default new Map([["../../assets/span.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_1ttgGN]]);
+		
