6853371d481722fd5176b0c2788e0de53b8d4b17
Francois Coleongco <christien.coleongco@gmail.com>
2026-04-09 11:29:16 -0400

    update projects

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,18 @@
 
 export const SITE_TITLE = 'fr4nsyz';
 export const SITE_DESCRIPTION = 'just another void pointer';
+
+// Add any GitHub repo URLs you want to show on the projects page.
+// Supports repos from any user/org.
+export const PROJECT_REPOS = [
+  'https://github.com/fr4nsyz/KernelHarbor',
+  'https://github.com/fr4nsyz/LoadBalancer',
+  'https://github.com/fr4nsyz/datascale',
+  'https://github.com/fr4nsyz/MEFSC',
+  'https://github.com/fr4nsyz/DNS_Tunneling',
+  'https://github.com/fr4nsyz/Steganography_Suite',
+  'https://github.com/fr4nsyz/Cocoon',
+  'https://github.com/fr4nsyz/KeepItRolling',
+  'https://github.com/fr4nsyz/SATD',
+  'https://github.com/fr4nsyz/Network-Encryption-Interface',
+];
